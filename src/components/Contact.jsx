f0be9aa15da5c2f1fea219266f21de5d14037b80
sach0318 <sachinyadv0318@gmail.com>
2025-05-24 15:28:09 +0530

    form

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,6 +1,33 @@
 import React from "react";
 
 const Contact = () => {
+
+    const [result, setResult] = React.useState("");
+
+    const onSubmit = async (event) => {
+      event.preventDefault();
+      setResult("Sending....");
+      const formData = new FormData(event.target);
+
+      formData.append("access_key", "your key");
+
+      const response = await fetch("https://api.web3forms.com/submit", {
+        method: "POST",
+        body: formData,
+      });
+
+      const data = await response.json();
+
+      if (data.success) {
+        setResult("");
+        alert("Form Submitted Successfully");
+        event.target.reset();
+      } else {
+        console.log("Error", data);
+        alert(data.message)
+        setResult("");
+      }
+    };
   return (
     <div
       className=" text-center p-6 py-20 lg:px-32 w-full overflow-hidden "
@@ -16,8 +43,8 @@ const Contact = () => {
         Ready to make a Move? Lets bild your future together
       </p>
 
-      <form className=" max-w-2xl mx-auto text-gray-600 pt-8    " >
-        <div className=" flex flex-wrap " >
+      <form className=" max-w-2xl mx-auto text-gray-600 pt-8    ">
+        <div className=" flex flex-wrap ">
           <div className=" w-full md:w-1/2 text-left ">
             Your Name
             <input
@@ -39,13 +66,22 @@ const Contact = () => {
             />
           </div>
         </div>
-        <div className="  my-6 text-left " >
-            message
-
-            <textarea  className=" w-full border border-gray-300 rounded py-3 px-4 mt-2 h-48 resize-none   " name="Message" placeholder="Message " required  ></textarea>
+        <div className="  my-6 text-left ">
+          message
+          <textarea
+            className=" w-full border border-gray-300 rounded py-3 px-4 mt-2 h-48 resize-none   "
+            name="Message"
+            placeholder="Message "
+            required
+          ></textarea>
         </div>
 
-
+        <button
+          onSubmit={onSubmit}
+          className=" bg-blue-600 text-white py-2 px-12 mb-10 rounded "
+        >
+          {result ? result : "Send Message"}
+        </button>
       </form>
     </div>
   );
